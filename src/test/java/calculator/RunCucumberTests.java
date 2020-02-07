2930fde9a880c7fd67a00dd335583a7ed0424395
Rubina Gill <msrubinagill@gmail.com>
2020-02-07 19:08:47 +0400

    test maintenance

--- src/test/java/calculator/RunCucumberTests.java
+++ src/test/java/calculator/RunCucumberTests.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.log4testng.Logger;
 
 import java.lang.reflect.Method;
